139e713bf5e44b46162653621ce37b14a06a51b8
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-11-27 15:32:45 +0100

    Add a nightly mode in the build script.

--- build-LIP6.sh
+++ build-LIP6.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
-    rootDir="${HOME}/coriolis-2.x"
+ nightlyInstall="false"
+ while [ $# -gt 0 ]; do
+   case $1 in
+     --nightly) echo "Installing in nightly mode.";
+                      nightlyInstall="true";;
+   esac
+   shift
+ done
+
+ if [ "${nightlyInstall}" = "true" ]; then
+   rootDir="${HOME}/nightly/coriolis-2.x"
+ else
+   rootDir="${HOME}/coriolis-2.x"
+ fi
    buildDir="${rootDir}/release/build-ihp"
  installDir="${rootDir}/release/install"
+
  rm -rf ${buildDir}
  rm -rf ${installDir}/lib64/python3.9/site-packages/pdks/ihpsg13g2
  meson setup --prefix ${installDir} ${buildDir}
