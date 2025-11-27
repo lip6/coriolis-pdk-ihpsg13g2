28fd0b1bed2491036c513ed356e92d11e0cf19ca
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-11-27 22:42:09 +0100

    Perform the "match" patch on the fly in build-LIP6.sh

--- build-LIP6.sh
+++ build-LIP6.sh
@@ -16,6 +16,13 @@
  fi
    buildDir="${rootDir}/release/build-ihp"
  installDir="${rootDir}/release/install"
+   patch001="`pwd`/packaging/replace-python-match.patch"
+
+ grep -Rl '^ *case ' IHP-Open-PDK/ihp-sg13g2/libs.tech/klayout/python/pycell4klayout-api/*
+ if [ $? -eq 0 ]; then
+   (cd IHP-Open-PDK/ihp-sg13g2/libs.tech/klayout/python/pycell4klayout-api;
+    patch -p1 < ${patch001})
+ fi
 
  rm -rf ${buildDir}
  rm -rf ${installDir}/lib64/python3.9/site-packages/pdks/ihpsg13g2
